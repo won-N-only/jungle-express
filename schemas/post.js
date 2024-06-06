f8b9b0d8541b289d6a284dc9575fcc3759b0ba26
won-n-only <siliver12@gmail.com>
2024-06-07 07:13:04 +0900

    feat: /schemas/posts.js - post schema에서 nickname추가, writer 및 password 제거

--- schemas/post.js
+++ schemas/post.js
@@ -3,9 +3,8 @@ const mongoose = require("mongoose");
 /** Post에는 [제목, 작성자명, 작성날짜, 비밀번호, 내용, 게시글id]이 있어야함 */
 const postSchema = new mongoose.Schema({
   title: {type: String, required: true},
-  writer: {type: String, required: true},
+  nickname: {type: String, required: true},
   date: {type: Date, required: true},
-  password: {type: String, required: true},
   content: {type: String, required: true},
 });
 
