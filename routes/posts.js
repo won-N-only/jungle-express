0d1c0cdc0efbbafc147032a89723a136ff42f6ec
won-n-only <siliver12@gmail.com>
2024-06-06 21:41:52 +0900

    fix: posts.js - 전체 게시글 조회 [제목, 작성자, 날짜]만 조회하게 변경

--- routes/posts.js
+++ routes/posts.js
@@ -5,7 +5,14 @@ const postSchema = require("../schemas/post.js");
 
 router.get("/", async (req, res) => {
   const posts = await postSchema.find({}).sort("-date");
-  res.json({posts});
+  const results = posts.map((post) => {
+    return {
+      title: title,
+      writer: writer,
+      date: date,
+    };
+  });
+  res.json({posts: results});
 });
 
 module.exports = router;
