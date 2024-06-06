23e009b5281f79954a32b8cda0a7d899f32b3d26
won-n-only <siliver12@gmail.com>
2024-06-07 05:30:20 +0900

    feat: routes/index.js, users.js - 유저 api구현을 위한 초기설정

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const postsRouter = require("./posts.js");
 const commentsRouter = require("./comments.js");
+const usersRouter = require("./users.js");
 
 router.get("/", (req, res) => {
   res.send("API에 오신걸 환영합니다"); // 루트 경로 요청 처리
@@ -10,5 +11,6 @@ router.get("/", (req, res) => {
 
 router.use("/posts", postsRouter); // ./api/posts 경로에 postsRouter 연결
 router.use("/comments", commentsRouter); // ./api/comments 경로에 commentsRouter 연결
+router.use("/users", usersRouter); // ./api/users 경로에 usersRouter 연결
 
 module.exports = router;
---
+++ routes/users.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const router = express.Router();
+
+const UserSchema = require("../schemas/user.js");
+
+module.exports = router;
