2b1a28322df3ca247ea8e50cb1b94223fbf844e5
won-n-only <siliver12@gmail.com>
2024-06-07 03:41:54 +0900

    feat: comments.js - 댓글 작성 구현

--- routes/comments.js
+++ routes/comments.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
+const postSchema = require("../schemas/post.js");
 const commentSchema = require("../schemas/comment.js");
 
 /** 댓글 목록 조회 */
@@ -12,6 +13,36 @@ router.get("/:postId", async (req, res) => {
   } catch (err) {
     console.error(err);
     res.status(500).json({errorMessage: "에러출동~~"});
+  }
+});
+
+/** 댓글 작성 */
+router.post("/:postId", async (req, res) => {
+  console.log("엘렐레");
+  const {postId} = req.params;
+  const {writer, password, content} = req.body;
+  const post = await postSchema.find({postId: postId});
+
+  if (!writer || !password || !content || !post) {
+    console.error("입력 안한게있네용");
+    return res.status(400).json({errorMessage: "입력 안한게 있네용"});
+  }
+
+  try {
+    console.log("댓글 작성 시도");
+    const createComment = await commentSchema.create({
+      writer: writer,
+      password: password,
+      date: new Date().toISOString(),
+      content: content,
+      postId: postId,
+    });
+    console.log("댓글 작 성성 공 ");
+
+    res.json({posts: createComment}).status(200);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({errorMessage: "  에 러 등 장"});
   }
 });
 
