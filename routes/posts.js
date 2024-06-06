0fe3d9487534b997104348e1bbb0f6e68ae52f77
won-n-only <siliver12@gmail.com>
2024-06-07 01:44:56 +0900

    fix: posts.js - 게시글 수정 시 비밀번호 요구하게 변경

--- routes/posts.js
+++ routes/posts.js
@@ -12,10 +12,12 @@ const postSchema = require("../schemas/post.js");
 /**  전체 게시글 목록 조회 */
 router.get("/", async (req, res) => {
   try {
+    console.log("전체 조회 시도 중");
     const posts = await postSchema
       .find({}, {title: 1, writer: 1, date: 1, _id: 1})
       .sort("-date");
 
+    console.log("전체 조회 성 공");
     res.json({posts: posts});
   } catch (err) {
     console.error(err);
@@ -33,8 +35,8 @@ router.post("/", async (req, res) => {
     return res.status(400).json({errMessage: "입력폼을 다 채워주세요"});
   }
 
-  console.log("입력 시도 중");
   try {
+    console.log("입력 시도 중");
     const createPost = await postSchema.create({
       title: title,
       writer: writer,
@@ -42,8 +44,8 @@ router.post("/", async (req, res) => {
       content: content,
       date: new Date().toISOString(),
     });
-
     console.log("입력 대성공");
+
     res.json({posts: createPost}).status(200);
   } catch (err) {
     console.error(err);
@@ -55,10 +57,8 @@ router.post("/", async (req, res) => {
 router.get("/:postId", async (req, res) => {
   try {
     console.log("조회 시도");
-
     const {postId} = req.params;
     const post = await postSchema.findById(postId);
-
     console.log("조회 대 성 공");
 
     res.json({posts: post}).status(200);
@@ -73,13 +73,19 @@ router.patch("/:postId", async (req, res) => {
   try {
     console.log("수정할 post 조회 시도");
     const {postId} = req.params;
+    const {password} = req.body;
     const post = await postSchema.findById(postId);
+
+    if (password != post.password || !post) {
+      const err = new Error();
+      throw err;
+    }
     const {content} = req.body;
-    console.log("조회 대 성 공");
 
     console.log("수정 시도");
     post.content = content;
     console.log("수정 성공");
+
     await post.save();
     res.status(200).json({posts: post});
   } catch (err) {
