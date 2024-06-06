1a260d1120d9e21c1ce2e2e7b0d383d839991f4e
won-n-only <siliver12@gmail.com>
2024-06-07 07:27:58 +0900

    feat: /schemas/comments.js -회원 인증을 위해 commentSchema 구조 수정

--- schemas/comment.js
+++ schemas/comment.js
@@ -2,8 +2,7 @@ const mongoose = require("mongoose");
 
 /** comments에는 [작성자명, 비밀번호, 작성날짜, 내용, 게시글id, 댓글id]가 있어야함 */
 const commentSchema = new mongoose.Schema({
-  writer: {type: String, required: true},
-  password: {type: String, required: true},
+  nickname: {type: String, required: true},
   date: {type: Date, required: true},
   content: {type: String, required: true},
   postId: {type: mongoose.Schema.Types.ObjectId, required: true},
