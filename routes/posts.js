9b7763df8e96d084574e6a2d4b796942421d1bcf
won-n-only <siliver12@gmail.com>
2024-06-06 22:49:03 +0900

    feat: posts.js - 게시글 작성 api 구현(#1)

--- routes/posts.js
+++ routes/posts.js
@@ -15,4 +15,24 @@ router.get("/", async (req, res) => {
   res.json({posts: results});
 });
 
+router.post("/", async (req, res) => {
+  const {title, writer, password, content} = req.body;
+
+  /** 나중에 valid check로 바꾸기 */
+  if (!title || !writer || !password || !content) {
+    console.log("입력 시도 실패");
+    return res.status(400).json({errMessage: "입력폼을 다 채워주세요"});
+  }
+
+  console.log("입력 시도 중");
+  const createPost = await postSchema.create({
+    title: title,
+    writer: writer,
+    password: password,
+    content: content,
+    date: new Date(),
+  });
+  console.log("입력 대성공");
+  res.json({posts: createPost}).status(200);
+});
 module.exports = router;
