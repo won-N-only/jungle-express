099d76f0f00a816cf2e00787349f713bc78e2b4f
won-n-only <siliver12@gmail.com>
2024-06-06 21:36:29 +0900

    feat: posts.js - 전체 게시글 조회 구현 (#1)

--- routes/posts.js
+++ routes/posts.js
@@ -1,4 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
+const postSchema = require("../schemas/post.js");
+
+router.get("/", async (req, res) => {
+  const posts = await postSchema.find({});
+  res.json({posts});
+});
+
 module.exports = router;
