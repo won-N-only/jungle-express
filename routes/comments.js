30ed182f6754dfc2c3eac6d8453f80589f2ca5ee
won-n-only <siliver12@gmail.com>
2024-06-07 02:44:07 +0900

    feat: comments.js - 댓글 목록 조회 구현

--- routes/comments.js
+++ routes/comments.js
@@ -1,6 +1,18 @@
 const express = require("express");
 const router = express.Router();
+
 const commentSchema = require("../schemas/comment.js");
 
+/** 댓글 목록 조회 */
+router.get("/:postId", async (req, res) => {
+  try {
+    const {postId} = req.params;
+    const comments = await commentSchema.find({postId: postId}).sort("-date");
+    res.status(200).json({comments: comments});
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({errorMessage: "에러출동~~"});
+  }
+});
 
 module.exports = router;
