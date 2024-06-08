42f31ff0a55a9c8b8fe3456705906c9d9b5d3102
won-n-only <siliver12@gmail.com>
2024-06-08 21:51:48 +0900

    feat: loaders/monoose.js - DB 연결 에러 catch

--- loaders/mongoose.js
+++ loaders/mongoose.js
@@ -2,6 +2,10 @@ const mongoose = require("mongoose");
 const config = require("../config/index");
 
 module.exports = async () => {
-  await mongoose.connect(config.mongoURI);
+  await mongoose.connect(config.mongoURI).catch((err) => console.error(err));
   console.log("MongoDB 연결 성공!");
+
+  mongoose.connection.on("error", (err) => {
+    console.error("몽고디비 연결 에러났어요 ", err);
+  });
 };
