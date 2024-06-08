ee13a70c4e0ca95437fd01b55851e72379a806ac
won-n-only <siliver12@gmail.com>
2024-06-09 01:32:09 +0900

    feat:  schemas/comments - 3layers위한 초기설정

--- schemas/comment.js
+++ schemas/comment.js
@@ -1,11 +1,21 @@
 const mongoose = require("mongoose");
 
 /** comments에는 [작성자명, 비밀번호, 작성날짜, 내용, 게시글id, 댓글id]가 있어야함 */
-const commentSchema = new mongoose.Schema({
-  nickname: {type: String, required: true},
-  date: {type: Date, required: true},
-  content: {type: String, required: true},
-  postId: {type: mongoose.Schema.Types.ObjectId, required: true},
-});
+const commentSchema = new mongoose.Schema(
+  {
+    nickname: {type: String, required: true},
+    date: {type: Date, required: true},
+    content: {type: String, required: true},
+    postId: {type: mongoose.Schema.Types.ObjectId, required: true},
+  },
+  {strict: true}
+);
 
-module.exports = mongoose.model("Comment", commentSchema);
+const CommentSchema = mongoose.model("comment", commentSchema);
+
+module.exports = class mongooseComment {
+  async getComment() {}
+  async postComment() {}
+  async updateComment() {}
+  async deleteComment() {}
+};
