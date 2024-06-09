30eaeb4343e8ae78648197752d3e0789650755f1
won-n-only <siliver12@gmail.com>
2024-06-09 16:24:19 +0900

    fix: objId 검증 로직 수정

--- middlewares/objid.js
+++ middlewares/objid.js
@@ -1,15 +1,15 @@
 const mongoose = require("mongoose");
 
-module.exports.postId = (req, res, next) => {
+module.exports.post = (req, res, next) => {
   const {postId} = req.params;
   if (!mongoose.isValidObjectId(postId)) {
     return res.status(400).json({errMessage: "object id가 이상함"});
   }
   next();
 };
-module.exports.commentId = (req, res, next) => {
-  const {postId} = req.params;
-  if (!mongoose.isValidObjectId(postId)) {
+module.exports.comm = (req, res, next) => {
+  const {commentId} = req.params;
+  if (!mongoose.isValidObjectId(commentId)) {
     return res.status(400).json({errMessage: "object id가 이상함"});
   }
   next();
